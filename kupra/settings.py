b8174cf868f31dd2495ffd2ecca353f8e68722d5
Adomas Jatužis <adomixaszvers@gmail.com>
2014-12-06 21:54:22 +0200

    Pridėtas pirmasis template

--- kupra/settings.py
+++ kupra/settings.py
@@ -81,3 +81,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(os.path.dirname(__file__), "templates"),
+)
+
